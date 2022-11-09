3307f3d902b0e9dd7dd52144e5f67147889d24ba
SC2003 <46285019+SC2003@users.noreply.github.com>
2022-11-09 18:34:04 +0530

    basic layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,27 @@
-const output = document.querySelector('#output');
-output.textContent = "Hello";
+const output = document.querySelector('#output1');
+// output.textContent = "Hello";
+
+const add = (num1, num2) => num1 + num2;
+const subtract = (num1, num2) => num1 - num2;
+const multiply = (num1, num2) => num1 * num2;
+const divide = (num1, num2) => num1 / num2;
+const power = (num1, num2) => num1 ** num2;
+
+const operate = (operator, num1, num2) => {
+    if(!operator || !num1 || !num2) return;
+    if (operator === '+') {
+        return add(num1, num2);
+    } else if (operator === '-') {
+        return subtract(num1, num2);
+    } else if (operator === '*') {
+        return multiply(num1, num2);
+   // } else if (operate === '/') {
+    //    return divide(num1, num2)
+    } else {
+        return divide(num1, num2)
+    }
+};
+let operator = '/';
+let num1 = 10;
+let num2 = 5;
+output.textContent = operate(operator, num1, num2);

3faa9b38b3ca1d592c61cc0c5b9e87f577b9c331
SC2003 <46285019+SC2003@users.noreply.github.com>
2022-11-10 16:19:16 +0530

    added some btn-event-listeners

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,12 +2,25 @@ const output1 = document.querySelector('#output1');
 const output2 = document.querySelector('#output2');
 const numBtn = document.querySelectorAll('.numbers');
 const opBtn = document.querySelectorAll('.operators');
-opBtn.forEach(btn => {
-    console.log(btn.value);
-    output2.textContent += btn.value;
-})
-
-
+const allClearBtn = document.querySelector('#all-clear');
+const clearBtn = document.querySelector('#clear');
+let operator = '+';
+let num1 = 10;
+let num2 = 5;
+numBtn.forEach(btn => {
+    
+    btn.addEventListener('click', (e) => {
+    output1.textContent += (btn.value);
+    });
+});
+allClearBtn.addEventListener('click', () => {
+    output1.textContent = null;;
+    output2.textContent = null;
+    operator = null;;
+    num1 = null;
+    num2 = null;
+});
+clearBtn.addEventListener('click', () => output1.textContent = output1.textContent.slice(0, -1));
 const add = (num1, num2) => num1 + num2;
 const subtract = (num1, num2) => num1 - num2;
 const multiply = (num1, num2) => num1 * num2;
@@ -28,7 +41,5 @@ const operate = (operator, num1, num2) => {
         return divide(num1, num2)
     }
 };
-let operator = '+';
-let num1 = 10;
-let num2 = 5;
-output1.textContent = operate(operator, num1, num2);
+
+output2.textContent = operate(operator, num1, num2);
